484517f68f98124ec687c0bf59e52e271b447ecf
dipc99 <dipalichaudhari99@gmail.com>
2023-01-29 03:00:26 +0530

    Repo3Project3

---
+++ src/main/java/com/BikkadIT/secondDemo/SecondDemoApplication.java
@@ -0,0 +1,15 @@
+package com.BikkadIT.secondDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondDemoApplication.class, args);
+		
+		System.out.println("SecondDemo Using Web");
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/secondDemo/SecondDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.secondDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
